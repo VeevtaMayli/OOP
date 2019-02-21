84f9d0ca804f5b29856bf706aae15734c0bd6b63
Ilya Matveev <matveeffilia@gmail.com>
2019-02-21 22:02:58 +0300

    Поиск строк в потоке вынесен в функцию поиска в файле

--- lw1/findtext/findtext.cpp
+++ lw1/findtext/findtext.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-vector<int> FindStringInStream(istream& in, string str)
+vector<int> FindStringInStream(istream& in, const string str)
 {
 	string tempStr;
 	vector<int> stringNumbers;
@@ -22,12 +22,27 @@ vector<int> FindStringInStream(istream& in, string str)
 			stringNumbers.push_back(stringNumber);
 		}
 	}
-
 	return stringNumbers;
+}
+
+vector<int> FindStringInFile(const string filePath, const string str, bool& err)
+{
+	ifstream input(filePath);
+
+	if (!input.is_open())
+	{
+		cout << "Failed to open " << filePath
+			 << " for reading" << endl;
+		err = true;
+		return {};
+	}
+	return FindStringInStream(input, str);
 }
 
 int main(int argc, char* argv[])
 {
+	bool err = false;
+
 	if (argc != 3)
 	{
 		cout << "Invalid arguments count" << endl
@@ -36,34 +51,18 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	ifstream input(argv[1]);
-
-	if (!input.is_open())
-	{
-		cout << "Failed to open " << argv[1]
-			 << " for reading" << endl;
-		return 1;
-	}
-
-	if (input.peek() == EOF)
-	{
-		cout << "Input file is empty" << endl;
-		return 1;
-	}
-
 	string searchedStr = argv[2];
 
-	if (searchedStr == "")
-	{
-		cout << "Text not found" << endl;
+	vector<int> foundStringNumbers = FindStringInFile(argv[1], searchedStr, err);
+	if (err)
 		return 1;
-	}
 
-	vector<int> foundedStringNumbers = FindStringInStream(input, searchedStr);
+	if (searchedStr == "")
+		foundStringNumbers.clear();
 
-	if (!foundedStringNumbers.empty())
+	if (!foundStringNumbers.empty())
 	{
-		copy(foundedStringNumbers.begin(), foundedStringNumbers.end(), ostream_iterator<int>(cout, "\n"));
+		copy(foundStringNumbers.begin(), foundStringNumbers.end(), ostream_iterator<int>(cout, "\n"));
 	}
 	else
 	{
--- lw1/findtext/test.bat
+++ lw1/findtext/test.bat
@@ -7,36 +7,30 @@ SET OUT="%TEMP%\output.txt"
 ::Проверка на количество передаваемых аргументов
 %PROGRAM% > %OUT%
 if NOT ERRORLEVEL 1 goto err
-fc %OUT% no-arguments-out.txt
-if ERRORLEVEL 1 goto err
+fc %OUT% no-arguments-out.txt || goto err
 
 ::Проверка на наличие входного файла
 %PROGRAM% somefile.txt "any string" > %OUT%
 if NOT ERRORLEVEL 1 goto err
-fc %OUT% no-input-file-out.txt
-if ERRORLEVEL 1 goto err
+fc %OUT% no-input-file-out.txt || goto err
 
-::Проверка на пустой входной файл
-%PROGRAM% empty.txt "any string" > %OUT%
-if NOT ERRORLEVEL 1 goto err
+::Проверка на пустой входной файла
+%PROGRAM% empty.txt "any string" > %OUT% || err
 fc %OUT% empty-out.txt
 if ERRORLEVEL 1 goto err
 
-::Проверка на пустую уискомую строку
-%PROGRAM% input.txt "" > %OUT%
-if NOT ERRORLEVEL 1 goto err
+::Проверка на пустую искомую строку
+%PROGRAM% input.txt "" > %OUT% || goto err
 fc %OUT% empty-string-out.txt
 if ERRORLEVEL 1 goto err
 
 ::Искомая строка не найдена
 %PROGRAM% input.txt "We are young" > %OUT% || goto err
-fc %OUT% nothing-found-out.txt
-if ERRORLEVEL 1 goto err
+fc %OUT% nothing-found-out.txt || goto err
 
 ::Иcкомая строка найдена
 %PROGRAM% input.txt "Last own loud" > %OUT% || goto err
-fc %OUT% string-found-out.txt
-if ERRORLEVEL 1 goto err
+fc %OUT% string-found-out.txt || goto err
 
 echo All tests passed
 exit /B 0
