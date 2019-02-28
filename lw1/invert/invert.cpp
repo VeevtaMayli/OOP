a11d85ee4d4c1867a9ec680fbbe245b8f71695e0
Ilya Matveev <matveeffilia@gmail.com>
2019-02-28 22:56:37 +0300

    Реализована invert, добавлены дополнительные тесты

--- lw1/invert/invert.cpp
+++ lw1/invert/invert.cpp
@@ -1,10 +1,12 @@
 ﻿#include "pch.h"
 #include <array>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <string>
 
 const int MATRIX_SIZE = 3;
+const double EPS = 0.0001;
 
 using namespace std;
 
@@ -19,19 +21,90 @@ bool TryToReadMatrixFromFile(string filePath, Matrix& matrix)
 		return false;
 	}
 
-	for (size_t i = 0; i < MATRIX_SIZE; i++)
+	for (size_t row = 0; row < MATRIX_SIZE; row++)
 	{
-		for (size_t j = 0; j < MATRIX_SIZE; j++)
+		for (size_t col = 0; col < MATRIX_SIZE; col++)
 		{
 			if (input.eof())
 			{
 				cout << "There is not 3x3 matrix in " << filePath << endl;
 				return false;
 			}
-			input >> matrix[i][j]; 
+			input >> matrix[row][col];
 		}
 	}
 	return true;
+}
+
+double GetSubdeterminant(const Matrix& matrix, const size_t row, const size_t col)
+{
+	array<double, 4> minorElements;
+	size_t currElement = 0;
+
+	for (size_t i = 0; i < MATRIX_SIZE; i++)
+	{
+		if (i == row)
+		{
+			continue;
+		}
+		for (size_t j = 0; j < MATRIX_SIZE; j++)
+		{
+			if (j == col)
+			{
+				continue;
+			}
+			minorElements[currElement] = matrix[i][j];
+			currElement++;
+		}
+	}
+	return minorElements[0] * minorElements[3] - minorElements[1] * minorElements[2];
+}
+
+double GetDeterminant(const Matrix& matrix)
+{
+	double determinant = 0;
+	const size_t row = 0;
+	short sign = 1;
+
+	for (size_t col = 0; col < MATRIX_SIZE; col++)
+	{
+		determinant += sign * matrix[row][col] * GetSubdeterminant(matrix, row, col);
+		sign *= -1;
+	}
+	return determinant;
+}
+
+bool TryToInvertMatrix(const Matrix& matrix, Matrix& invertedMatrix)
+{
+	double determinant = GetDeterminant(matrix);
+	if (fabs(determinant - 0) < EPS)
+	{
+		cout << "The determinant of the matrix is 0 so the inverse matrix does not exist" << endl;
+		return false;
+	}
+
+	short sign = 1;
+	for (size_t row = 0; row < MATRIX_SIZE; row++)
+	{
+		for (size_t col = 0; col < MATRIX_SIZE; col++)
+		{
+			invertedMatrix[col][row] = sign * GetSubdeterminant(matrix, row, col) / determinant;
+			sign *= -1;
+		}
+	}
+	return true;
+}
+
+void PrintMatrix(const Matrix& matrix)
+{
+	for (size_t i = 0; i < MATRIX_SIZE; i++)
+	{
+		for (size_t j = 0; j < MATRIX_SIZE; j++)
+		{
+			cout << fixed << setprecision(3) << matrix[i][j] << " ";
+		}
+		cout << endl;
+	}
 }
 
 int main(int argc, char* argv[])
@@ -50,5 +123,14 @@ int main(int argc, char* argv[])
 	{
 		return 1;
 	}
-	
+
+	Matrix invertedMatrix;
+
+	if (!TryToInvertMatrix(inputMatrix, invertedMatrix))
+	{
+		return 1;
+	}
+
+	PrintMatrix(invertedMatrix);
+	return 0;
 }
--- lw1/invert/test.bat
+++ lw1/invert/test.bat
@@ -19,6 +19,22 @@ fc %OUT% no-input-file-out.txt || goto err
 if NOT ERRORLEVEL 1 goto err
 fc %OUT% empty-out.txt || goto err
 
+::Проверка на некорректную матрицу
+%PROGRAM% invalid-matrix.txt > %OUT%
+if NOT ERRORLEVEL 1 goto err
+fc %OUT% invalid-matrix-out.txt || goto err
+
+::Матрица с определителем < 0
+%PROGRAM% zero-det.txt > %OUT% || err
+fc %OUT% zero-det-out.txt || goto err
+
+::Матрица только с положительными значениями с определителем > 0
+%PROGRAM% positive-matrix.txt > %OUT% || err
+fc %OUT% positive-matrix-out.txt || goto err
+
+::Матрица с любыми значениями с определителем > 0
+%PROGRAM% real-matrix.txt > %OUT% || err
+fc %OUT% real-matrix-out.txt || goto err
 echo All tests passed
 exit /B 0
 
