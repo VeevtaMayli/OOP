87d9a96618a01a9b3ef33f3a65eda5f1dfb947f0
Ilya Matveev <matveeffilia@gmail.com>
2019-03-09 21:03:25 +0300

    Add program which encoding cin to cout by HtmlEncode

--- lw2/html_encode/main.cpp
+++ lw2/html_encode/main.cpp
@@ -1,7 +1,20 @@
 #include "pch.h"
 #include "html_encode.h"
 
+using namespace std;
+
+void HtmlEncodeStream(istream& input, ostream& output)
+{
+	string line;
+	
+	while (getline(input, line))
+	{
+		output << HtmlEncode(line) << endl;
+	}
+}
+
 int main()
 {
+	HtmlEncodeStream(cin, cout);	
 	return 0;
 }
