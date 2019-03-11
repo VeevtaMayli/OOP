c308ae0b1b7dbdbf30e1a4db36ad18e826b7f7ed
Ilya Matveev <matveeffilia@gmail.com>
2019-03-11 22:28:58 +0300

    Add dictionary class

--- lw2/dictionary/dictionary.cpp
+++ lw2/dictionary/dictionary.cpp
@@ -1,3 +1,51 @@
 ﻿#include "pch.h"
 #include "dictionary.h"
 
+Dictionary::Dictionary()
+{
+}
+
+Dictionary::Dictionary(const std::string& fileName)
+{
+}
+
+std::string Dictionary::Translate() const
+{
+	return std::string();
+}
+
+void Dictionary::AddTranslation(const std::string& word, const std::string& translation)
+{
+}
+
+bool Dictionary::Save() const
+{
+	return false;
+}
+
+bool Dictionary::Save(const std::string& fileName) const
+{
+	return false;
+}
+
+bool Dictionary::WasChanged() const
+{
+	return false;
+}
+
+void Dictionary::ReadFromFile(std::istream& input)
+{
+}
+
+void Dictionary::SaveToFile(std::ostream& output) const
+{
+}
+
+std::string Dictionary::ToLower(const std::string& str) const
+{
+	return std::string();
+}
+
+void Dictionary::InsertIntoDictionary(const std::string& word, const std::string& translation)
+{
+}
--- lw2/dictionary/dictionary.h
+++ lw2/dictionary/dictionary.h
@@ -1,1 +1,27 @@
 #pragma once
+
+#include <map>
+
+using TranslationStorage = std::multimap<std::string, std::string>;
+
+class Dictionary
+{
+public:
+	Dictionary();
+	Dictionary(const std::string& fileName);
+	std::string Translate() const;
+	void AddTranslation(const std::string& word, const std::string& translation);
+	bool Save() const;
+	bool Save(const std::string& fileName) const;
+	bool WasChanged() const;
+
+private:
+	void ReadFromFile(std::istream& input);
+	void SaveToFile(std::ostream& output) const;
+	std::string ToLower(const std::string& str) const;
+	void InsertIntoDictionary(const std::string& word, const std::string& translation);
+
+	TranslationStorage m_storage;
+	const std::string m_dictionaryFileName;
+	bool m_wasChanged;
+};
