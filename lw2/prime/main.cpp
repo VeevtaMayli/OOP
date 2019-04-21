ce483b51673912622df8463b32a60d1a936190c2
Ilya Matveev <matveeffilia@gmail.com>
2019-04-21 10:45:38 +0300

    Implement program using prime number generator

--- lw2/prime/main.cpp
+++ lw2/prime/main.cpp
@@ -1,6 +1,41 @@
 #include "pch.h"
+#include "prime.h"
 
-int main()
+using namespace std;
+
+const uint32_t MAX_UPPER_BOUND = 100000000;
+
+bool ParseCommandLine(int argc, char* argv[], uint32_t& upperBound)
 {
-	std::cout << "Hello World!\n";
+	if (argc != 2)
+	{
+		return false;
+	}
+	try
+	{
+		upperBound = stoul(argv[1]);
+	}
+	catch (const exception& e)
+	{
+		cout << e.what() << endl;
+		return false;
+	}
+	return upperBound <= MAX_UPPER_BOUND;
+}
+
+void PrintSet(const PrimeSet& s)
+{
+	copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+}
+
+int main(int argc, char* argv[])
+{
+	uint32_t upperBound;
+	if (!ParseCommandLine(argc, argv, upperBound))
+	{
+		cout << "Invalid upper bound of numbers. Correct value is less then " << MAX_UPPER_BOUND << endl;
+		return 1;
+	}
+	PrintSet(GeneratePrimeNumbersSet(upperBound));
+	return 0;
 }
