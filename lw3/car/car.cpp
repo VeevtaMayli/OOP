266dd6d541a3307a6204bc39d3340643ef09777f
Ilya Matveev <matveeffilia@gmail.com>
2019-04-28 10:44:39 +0300

    Add CCar

--- lw3/car/car.cpp
+++ lw3/car/car.cpp
@@ -1,2 +1,52 @@
 ﻿#include "pch.h"
 #include "car.h"
+
+using namespace std;
+
+std::map<int, TransmitionSpeedRange> CCar::m_transmition = { { -1, { 0, 20 } },
+	{ 0, { 0, 0 } },
+	{ 1, { 0, 30 } },
+	{ 2, { 20, 50 } },
+	{ 3, { 30, 60 } },
+	{ 4, { 40, 90 } },
+	{ 5, { 50, 150 } } };
+
+bool CCar::TurnOnEngine()
+{
+	return false;
+}
+
+bool CCar::TurnOffEngine()
+{
+	return false;
+}
+
+bool CCar::SetGear(const int gear)
+{
+	return false;
+}
+
+bool CCar::SetSpeed(const int speed)
+{
+	return false;
+}
+
+int CCar::GetGear() const
+{
+	return m_gear;
+}
+
+int CCar::GetSpeed() const
+{
+	return m_speed;
+}
+
+Direction CCar::GetDirection() const
+{
+	return (m_speed != 0) ? ((m_speed > 0) ? Direction::FORWARD : Direction::BACK) : Direction::NONE;
+}
+
+EngineState CCar::GetEngineState() const
+{
+	return m_engineState	;
+}
--- lw3/car/car.h
+++ lw3/car/car.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include <map>
+
+struct TransmitionSpeedRange
+{
+	unsigned int min;
+	unsigned int max;
+
+};
+
+enum class EngineState
+{
+	ON,
+	OFF
+};
+
+enum class Direction
+{
+	FORWARD,
+	BACK,
+	NONE
+};
+
+class CCar
+{
+public:
+	bool TurnOnEngine();
+	bool TurnOffEngine();
+	bool SetGear(const int gear);
+	bool SetSpeed(const int speed);
+	int GetGear() const;
+	int GetSpeed() const;
+	Direction GetDirection() const;
+	EngineState GetEngineState() const;
+
+private:
+	EngineState m_engineState = EngineState::OFF;
+	Direction m_direction = Direction::NONE;
+	int m_speed = 0;
+	int m_gear = 0;
+	static std::map<int, TransmitionSpeedRange> m_transmition;
+};
