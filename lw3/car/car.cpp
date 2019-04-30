de25cefa00a40456e82f7c2aec8f27b9446adc18
Ilya Matveev <matveeffilia@gmail.com>
2019-04-30 22:06:33 +0300

    Implement CCar methods

--- lw3/car/car.cpp
+++ lw3/car/car.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-std::map<int, TransmitionSpeedRange> CCar::m_transmition = { { -1, { 0, 20 } },
-	{ 0, { 0, 0 } },
+map<int, TransmitionSpeedRange> CCar::m_transmition = { { -1, { 0, 20 } },
+	{ 0, { 0, 150 } },
 	{ 1, { 0, 30 } },
 	{ 2, { 20, 50 } },
 	{ 3, { 30, 60 } },
@@ -13,22 +13,98 @@ std::map<int, TransmitionSpeedRange> CCar::m_transmition = { { -1, { 0, 20 } },
 
 bool CCar::TurnOnEngine()
 {
-	return false;
+	m_engineState = EngineState::ON;
+	return true;
 }
 
 bool CCar::TurnOffEngine()
 {
+	if (m_engineState == EngineState::ON && m_gear == 0 && m_speed == 0)
+	{
+		m_engineState = EngineState::OFF;
+		return true;
+	}
 	return false;
+}
+
+bool CCar::CanShiftGear(const int gear) const
+{
+	if (m_engineState == EngineState::OFF)
+	{
+		return false;
+	}
+	if (gear > 0 && m_direction == Direction::BACK || gear < 0 && m_direction != Direction::NONE)
+	{
+		return false;
+	}
+	try
+	{
+		TransmitionSpeedRange range = m_transmition.at(gear);
+		if (range.min > m_speed || range.max < m_speed)
+		{
+			return false;
+		}
+	}
+	catch (std::out_of_range const& e)
+	{
+		e.what();
+		return false;
+	}
+	return true;
+}
+
+bool CCar::CanSetSpeed(const unsigned int speed) const
+{
+	if (m_engineState == EngineState::OFF)
+	{
+		return false;
+	}
+	if (m_gear == 0 && speed > m_speed)
+	{
+		return false;
+	}
+	if (m_transmition[m_gear].min > speed || m_transmition[m_gear].max < speed)
+	{
+		return false;
+	}
+	return true;
+}
+
+void CCar::ChangeDirection()
+{
+	if (m_speed == 0)
+	{
+		m_direction = Direction::NONE;
+	}
+	if (m_speed > 0 && m_gear > 0)
+	{
+		m_direction = Direction::FORWARD;
+	}
+	if (m_speed > 0 && m_gear < 0)
+	{
+		m_direction = Direction::FORWARD;
+	}
 }
 
 bool CCar::SetGear(const int gear)
 {
-	return false;
+	if (!CanShiftGear(gear))
+	{
+		return false;
+	}
+	m_gear = gear;
+	return true;
 }
 
 bool CCar::SetSpeed(const int speed)
 {
-	return false;
+	if (!CanSetSpeed(speed))
+	{
+		return false;
+	}
+	m_speed = speed;
+	ChangeDirection();
+	return true;
 }
 
 int CCar::GetGear() const
@@ -36,17 +112,17 @@ int CCar::GetGear() const
 	return m_gear;
 }
 
-int CCar::GetSpeed() const
+unsigned int CCar::GetSpeed() const
 {
 	return m_speed;
 }
 
 Direction CCar::GetDirection() const
 {
-	return (m_speed != 0) ? ((m_speed > 0) ? Direction::FORWARD : Direction::BACK) : Direction::NONE;
+	return m_direction;
 }
 
 EngineState CCar::GetEngineState() const
 {
-	return m_engineState	;
+	return m_engineState;
 }
--- lw3/car/car.h
+++ lw3/car/car.h
@@ -6,7 +6,6 @@ struct TransmitionSpeedRange
 {
 	unsigned int min;
 	unsigned int max;
-
 };
 
 enum class EngineState
@@ -30,14 +29,18 @@ public:
 	bool SetGear(const int gear);
 	bool SetSpeed(const int speed);
 	int GetGear() const;
-	int GetSpeed() const;
+	unsigned int GetSpeed() const;
 	Direction GetDirection() const;
 	EngineState GetEngineState() const;
 
 private:
+	bool CanShiftGear(const int gear) const;
+	bool CanSetSpeed(const unsigned int speed) const;
+	void ChangeDirection();
+
 	EngineState m_engineState = EngineState::OFF;
 	Direction m_direction = Direction::NONE;
-	int m_speed = 0;
+	unsigned int m_speed = 0;
 	int m_gear = 0;
 	static std::map<int, TransmitionSpeedRange> m_transmition;
 };
