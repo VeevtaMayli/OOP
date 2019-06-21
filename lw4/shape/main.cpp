bd403bedc1240e9cc27ccfa976f80d4dd7f66c58
Ilya Matveev <matveeffilia@gmail.com>
2019-06-22 02:26:26 +0300

    Add program using IShape and CShapeProcessor

--- lw4/shape/main.cpp
+++ lw4/shape/main.cpp
@@ -1,6 +1,30 @@
 #include "pch.h"
+#include "CShapeProcessor.h"
+#include "IShape.h"
 
-int main()
+using namespace std;
+
+int main(int argc, char* argv[])
 {
+	try
+	{
+		CShapeProcessor processor(cin, cout);
+		while (!cin.eof() && !cin.fail())
+		{
+			if (!processor.HandleCommand())
+			{
+				cout << "Unknown command!\nUsage: Circle, Trinagle, Rectangle or LineSegment with parameters" << endl;
+			}
+		}
+		processor.PrintShapeWithMaxArea();
+		processor.PrintShapeWithMinPerimeter();
+	}
+	catch (const exception& e)
+	{
+		cerr << e.what() << endl;
+
+		return 1;
+	}
+
 	return 0;
 }
