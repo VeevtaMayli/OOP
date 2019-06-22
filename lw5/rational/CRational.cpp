601a23af7860ed1e626661a436ec774ba682d879
Ilya Matveev <matveeffilia@gmail.com>
2019-06-22 07:59:32 +0300

    Add methods definition to CRational

--- lw5/rational/CRational.cpp
+++ lw5/rational/CRational.cpp
@@ -1,6 +1,113 @@
 #include "pch.h"
 #include "CRational.h"
 
-CRational::CRational()
+CRational::CRational(int numerator, int denominator)
+	: m_numerator(numerator)
+	, m_denominator(denominator)
 {
+}
+
+int CRational::GetNumerator() const
+{
+	return m_numerator;
+}
+
+int CRational::GetDenominator() const
+{
+	return m_denominator;
+}
+
+double CRational::ToDouble() const
+{
+	return static_cast<double>(m_numerator) / m_denominator;
+}
+
+CRational const CRational::operator-() const
+{
+	return nullptr;
+}
+
+CRational const CRational::operator+() const
+{
+	return nullptr;
+}
+
+CRational& CRational::operator+=(const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+CRational& CRational::operator-=(const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+CRational& CRational::operator*=(const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+CRational& CRational::operator/=(const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+CRational const operator+(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+CRational const operator-(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+CRational const operator*(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+CRational const operator/(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator==(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator!=(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator<(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator>(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator<=(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+bool const operator>=(const CRational& fraction1, const CRational& fraction2)
+{
+	return nullptr;
+}
+
+std::ostream& operator<<(std::ostream& strm, const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
+}
+
+std::istream& operator>>(std::istream& strm, const CRational& fraction)
+{
+	// TODO: вставьте здесь оператор return
 }
--- lw5/rational/CRational.h
+++ lw5/rational/CRational.h
@@ -2,5 +2,34 @@
 class CRational
 {
 public:
-	CRational();
+	CRational(int numerator = 0, int denominator = 1);
+
+	int GetNumerator() const;
+	int GetDenominator() const;
+	double ToDouble() const;
+
+	CRational const operator-() const;
+	CRational const operator+() const;
+	CRational& operator+=(const CRational& fraction);
+	CRational& operator-=(const CRational& fraction);
+	CRational& operator*=(const CRational& fraction);
+	CRational& operator/=(const CRational& fraction);
+
+private:
+	int m_numerator, m_denominator;
 };
+
+CRational const operator+(const CRational& fraction1, const CRational& fraction2);
+CRational const operator-(const CRational& fraction1, const CRational& fraction2);
+CRational const operator*(const CRational& fraction1, const CRational& fraction2);
+CRational const operator/(const CRational& fraction1, const CRational& fraction2);
+
+bool const operator==(const CRational& fraction1, const CRational& fraction2);
+bool const operator!=(const CRational& fraction1, const CRational& fraction2);
+bool const operator<(const CRational& fraction1, const CRational& fraction2);
+bool const operator>(const CRational& fraction1, const CRational& fraction2);
+bool const operator<=(const CRational& fraction1, const CRational& fraction2);
+bool const operator>=(const CRational& fraction1, const CRational& fraction2);
+
+std::ostream& operator<<(std::ostream& strm, const CRational& fraction);
+std::istream& operator>>(std::istream& strm, const CRational& fraction);
