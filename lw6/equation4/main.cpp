1a9c9cdde3f2d109391faa86a06551ab9d881b76
Ilya Matveev <matveeffilia@gmail.com>
2019-06-23 12:56:12 +0300

    Add program using Solve4()

--- lw6/equation4/main.cpp
+++ lw6/equation4/main.cpp
@@ -1,6 +1,26 @@
 ﻿#include "pch.h"
+#include "equation4.h"
+
+bool GetCoefficients(std::istream& inStrm, double& a, double& b, double& c, double& d, double& e)
+{
+	return (inStrm >> a) && (inStrm >> b) && (inStrm >> c) && (inStrm >> d) && (inStrm >> e);
+}
 
 int main()
 {
-    
+	double a, b, c, d, e;
+	while (GetCoefficients(std::cin, a, b, c, d, e))
+	{
+		try
+		{
+			EquationRoot roots = Solve4(a, b, c, d, e);
+			std::copy(roots.begin(), roots.end(), std::ostream_iterator<double>(std::cout, " "));
+			std::cout << std::endl;
+		}
+		catch (const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	return 0;
 }
