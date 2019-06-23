2ed19f416b915a7fdc6719e8059147e94df97bf2
Ilya Matveev <matveeffilia@gmail.com>
2019-06-24 01:03:56 +0300

    Implement FinMaxEx()

--- lw7/FindMaxEx/FindMaxEx.h
+++ lw7/FindMaxEx/FindMaxEx.h
@@ -1,1 +1,24 @@
 #pragma once
+#include <functional>
+#include <vector>
+
+template <typename T, typename Less>
+bool FindMaxEx(std::vector<T> const& arr, T& maxValue, Less const& less)
+{
+	if (arr.empty())
+	{
+		return false;
+	}
+
+	auto maxIterator = arr.begin();
+	for (auto it = arr.begin(); it != arr.end(); ++it)
+	{
+		if (less(*maxIterator, *it))
+		{
+			maxIterator = it;
+		}
+	}
+
+	maxValue = *maxIterator;
+	return true;
+}
