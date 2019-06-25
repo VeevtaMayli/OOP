a1a6bfc5c82d79aebc0af3a7213584c50a960768
Ilya Matveev <matveeffilia@gmail.com>
2019-06-25 22:38:27 +0300

    Add structure of CMyStack

--- lw7/my_stack/CMyStack.h
+++ lw7/my_stack/CMyStack.h
@@ -1,1 +1,69 @@
 #pragma once
+
+template <typename T>
+class CMyStack
+{
+	struct Element
+	{
+		Element(const T& data, Element* next)
+			: data(data)
+			, next(next)
+		{
+		}
+		T data;
+		Element* next;
+	};
+
+public:
+	CMyStack() = default;
+
+	CMyStack(const CMyStack& other)
+	{
+	}
+
+	CMyStack(CMyStack&& stack)
+	{
+	}
+
+	~CMyStack()
+	{
+	}
+
+	bool IsEmpty() const
+	{
+	
+	}
+
+	void Push(const T& data)
+	{
+
+	}
+
+	void Pop()
+	{
+
+	}
+
+	const T& GetTop() const
+	{
+
+	}
+
+	void Clear()
+	{
+
+	}
+
+	CMyStack& operator=(const CMyStack& other)
+	{
+
+	}
+
+	CMyStack& operator=(CMyStack&& other)
+	{
+
+	}
+
+private:
+	Element* m_top = nullptr;
+};
