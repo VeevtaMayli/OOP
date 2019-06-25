8f38f23e0ac49e3fcae7967038835545a61424db
Ilya Matveev <matveeffilia@gmail.com>
2019-06-26 00:06:39 +0300

    Implement IsEmpty(), Push(), Pop(), GetTop() of CMyStack

--- lw7/my_stack/CMyStack.h
+++ lw7/my_stack/CMyStack.h
@@ -29,24 +29,41 @@ public:
 	{
 	}
 
-	bool IsEmpty() const
+	bool IsEmpty() const noexcept
 	{
-	
+		return !m_top;
 	}
 
 	void Push(const T& data)
 	{
-
+		Element* newElement = new Element(data, m_top);
+		m_top = newElement;
 	}
 
 	void Pop()
 	{
-
+		if (!IsEmpty())
+		{
+			Element* ptr = m_top;
+			m_top = m_top->next;
+			delete ptr;
+		}
+		else
+		{
+			throw std::out_of_range("Stack is empty");
+		}
 	}
 
 	const T& GetTop() const
 	{
-
+		if (!IsEmpty())
+		{
+			return m_top->data;
+		}
+		else
+		{
+			throw std::out_of_range("Stack is empty");
+		}
 	}
 
 	void Clear()
