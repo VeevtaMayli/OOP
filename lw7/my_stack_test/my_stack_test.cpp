6196a32b5b85172d9ba83b4d102c4b8b6a45fef5
Ilya Matveev <matveeffilia@gmail.com>
2019-06-27 14:20:27 +0300

    Add tests for CMyStack

--- lw7/my_stack_test/my_stack_test.cpp
+++ lw7/my_stack_test/my_stack_test.cpp
@@ -1,7 +1,151 @@
 ﻿#include "pch.h"
 #include "lw7/my_stack/CMyStack.h"
 
-TEST_CASE("")
-{
+using namespace std;
 
+template <typename T>
+void IsEqual(CMyStack<T>& stack, CMyStack<T>& copy)
+{
+	while (!stack.IsEmpty())
+	{
+		CHECK(stack.GetTop() == copy.GetTop());
+		stack.Pop();
+		copy.Pop();
+	}
+}
+
+TEST_CASE("stack of int")
+{
+	CMyStack<int> stack;
+
+	SECTION("get top and pop of empty stack")
+	{
+		CHECK(stack.IsEmpty());
+		CHECK_THROWS_WITH(stack.GetTop(), "Stack is empty");
+		CHECK_THROWS_WITH(stack.Pop(), "Stack is empty");
+	}
+
+	SECTION("push one element, get top and pop")
+	{
+		stack.Push(1);
+		CHECK_FALSE(stack.IsEmpty());
+		CHECK(stack.GetTop() == 1);
+		CHECK_NOTHROW(stack.Pop());
+		CHECK_THROWS_WITH(stack.GetTop(), "Stack is empty");
+		CHECK(stack.IsEmpty());
+	}
+
+	SECTION("push some elements")
+	{
+		stack.Push(1);
+		stack.Push(2);
+		stack.Push(3);
+		CHECK_FALSE(stack.IsEmpty());
+
+		SECTION("using copy constructor")
+		{
+			CMyStack<int> copy(stack);
+			CHECK_FALSE(stack.IsEmpty());
+			IsEqual(stack, copy);
+		}
+
+		SECTION("using copy assignment operator")
+		{
+			CMyStack<int> copy = stack;
+			CHECK_FALSE(stack.IsEmpty());
+			IsEqual(stack, copy);
+		}
+
+		SECTION("using move constructor")
+		{
+			CMyStack<int> copy = stack;
+			CMyStack<int> other(move(stack));
+
+			CHECK(stack.IsEmpty());
+			IsEqual(copy, other);
+		}
+
+		SECTION("using move assignment operator")
+		{
+			CMyStack<int> copy = stack;
+			CMyStack<int> other = move(stack);
+
+			CHECK(stack.IsEmpty());
+			IsEqual(copy, other);
+		}
+
+		SECTION("clear stack")
+		{
+			CHECK_NOTHROW(stack.Clear());
+			CHECK(stack.IsEmpty());
+		}
+	}
+}
+
+TEST_CASE("stack of string")
+{
+	CMyStack<string> stack;
+
+	SECTION("get top and pop of empty stack")
+	{
+		CHECK(stack.IsEmpty());
+		CHECK_THROWS_WITH(stack.GetTop(), "Stack is empty");
+		CHECK_THROWS_WITH(stack.Pop(), "Stack is empty");
+	}
+
+	SECTION("push one element, get top and pop")
+	{
+		stack.Push("test");
+		CHECK_FALSE(stack.IsEmpty());
+		CHECK(stack.GetTop() == "test");
+		CHECK_NOTHROW(stack.Pop());
+		CHECK_THROWS_WITH(stack.GetTop(), "Stack is empty");
+		CHECK(stack.IsEmpty());
+	}
+
+	SECTION("push some elements")
+	{
+		stack.Push("test");
+		stack.Push("my");
+		stack.Push("stack");
+		CHECK_FALSE(stack.IsEmpty());
+
+		SECTION("using copy constructor")
+		{
+			CMyStack<string> copy(stack);
+			CHECK_FALSE(stack.IsEmpty());
+			IsEqual(stack, copy);
+		}
+
+		SECTION("using copy assignment operator")
+		{
+			CMyStack<string> copy = stack;
+			CHECK_FALSE(stack.IsEmpty());
+			IsEqual(stack, copy);
+		}
+
+		SECTION("using move constructor")
+		{
+			CMyStack<string> copy = stack;
+			CMyStack<string> other(move(stack));
+
+			CHECK(stack.IsEmpty());
+			IsEqual(copy, other);
+		}
+
+		SECTION("using move assignment operator")
+		{
+			CMyStack<string> copy = stack;
+			CMyStack<string> other = move(stack);
+
+			CHECK(stack.IsEmpty());
+			IsEqual(copy, other);
+		}
+
+		SECTION("clear stack")
+		{
+			CHECK_NOTHROW(stack.Clear());
+			CHECK(stack.IsEmpty());
+		}
+	}
 }
