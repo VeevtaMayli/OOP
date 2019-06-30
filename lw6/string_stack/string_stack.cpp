b698a943bb9036897f8a2a93a0e1f19871339a75
Ilya Matveev <matveeffilia@gmail.com>
2019-06-30 09:12:03 +0300

    Implement constructors & some methods of CStringStack

--- lw6/string_stack/string_stack.cpp
+++ lw6/string_stack/string_stack.cpp
@@ -9,17 +9,18 @@ CStringStack::Element::Element(const std::string& data, std::unique_ptr<Element>
 
 CStringStack::CStringStack(const CStringStack& other)
 {
-
+	*(this) = other;
 }
 
 CStringStack::CStringStack(CStringStack&& other)
+	: m_top(std::move(other.m_top))
 {
-
+		other.m_top = nullptr;
 }
 
 CStringStack::~CStringStack()
 {
-
+	Clear();
 }
 
 CStringStack& CStringStack::operator=(const CStringStack& other)
@@ -39,20 +40,31 @@ void CStringStack::Push(const std::string& data)
 
 bool CStringStack::IsEmpty() const
 {
-
+	return !m_top;
 }
 
 std::string CStringStack::GetTop() const
 {
-
+	if (IsEmpty())
+	{
+		throw std::out_of_range("Stack is empty");
+	}
+	return m_top->data;
 }
 
 void CStringStack::Pop()
 {
-
+	if (IsEmpty())
+	{
+		throw std::out_of_range("Stack is empty");
+	}
+	m_top = std::move(m_top->next);
 }
 
 void CStringStack::Clear()
 {
-
+	while (!IsEmpty())
+	{
+		Pop();
+	}
 }
